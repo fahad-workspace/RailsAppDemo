643b35ae8c38ac0b11d030cc42b3d7ab0f523acc
Fahad Sarwar <fahad.workspace@outlook.com>
2014-08-19 09:56:03 +0530

    Check

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,15 +44,13 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  #after :publishing, :restart
+  after :publishing, :restart
   
-  after 'deploy:publishing', 'deploy:restart'
-  
-  puts "I AM HERE"
+  #after 'deploy:publishing', 'deploy:restart'
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
