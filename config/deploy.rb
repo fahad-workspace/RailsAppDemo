3c19e7d35f1e4500bc2620919abad1a5a7d55894
Fahad Sarwar <fahad.workspace@outlook.com>
2014-08-19 11:52:06 +0530

    Check

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :deploy do
 				# Your restart mechanism here, for example:
 				path = release_path.join('tmp/StartWebApp.sh')
 				execute :touch, path
-				echo "RAILS_ENV=staging rails server" >> myscript.sh
+				echo "RAILS_ENV=staging rails server" >> path
 				echo "2nd Line" >> myscript.sh
 				puts "I AM HERE"
 			end
