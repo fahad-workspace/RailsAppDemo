d975c65a84e0258ee3fd0bc516061720cbaad3b5
Fahad Sarwar <fahad.workspace@outlook.com>
2014-08-19 12:18:31 +0530

    Check

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,8 +46,7 @@ namespace :deploy do
 			on roles(:app), in: :sequence, wait: 5 do
 				# Your restart mechanism here, for example:
 				path = release_path.join('StartWebApp.sh')
-				execute "printf '#!/bin/bash' > #{path}"
-				execute "printf 'RAILS_ENV=staging rails server' > #{path}"
+				execute "printf '#!/bin/bash\ncd #{release_path}\nRAILS_ENV=staging rails server' > #{path}"
 				execute "bash #{path}"
 			end
     end
